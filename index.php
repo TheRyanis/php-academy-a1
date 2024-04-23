4c58b47ab0c523bf66901300a3ad287cae45fb4d
adam.brix <adam.brix@student.howest.be>
2024-04-23 21:29:24 +0200

    waiting for code review

--- index.php
+++ index.php
@@ -1,21 +1,62 @@
 <?php
 
-//Default time zone set from php manual https://www.php.net/manual/en/timezones.europe.php;
+//Default time zone set from PHP manual https://www.php.net/manual/en/timezones.europe.php;
 date_default_timezone_set("Europe/Bratislava");
 
 // Time with this format saved in a variable 2023-12-07 23:59:59;
-$timeFormat = date("Y/m/d H:i:s");
+$timeFormat = date("Y-m-d H:i:s");
 echo $timeFormat;
 
-$logFilePath = "log.txt";
+$logFilePath = "students.json";
 
-if (isset($_POST['name'])) {
-    $data = $_POST['name'];
-    file_put_contents($logFilePath, $data, FILE_APPEND);
+if (date('H') >= 20 && date('H') <= 0) {
+    die("You cannot enter school between 20 and 24");
 }
 
+function isLateMorning()
+{
+    $hour = date("H");
+    return ($hour >= 8);
+}
 
+function logStudent($name, $late)
+{
+    global $logFilePath, $timeFormat;
 
+    if (!empty($name)) {
+        $logData = file_exists($logFilePath) ? json_decode(file_get_contents($logFilePath), true) : [];
+
+        $logData[] = array(
+            'name' => $name,
+            'time' => $timeFormat,
+            'late' => $late ? 'Yes' : 'no'
+        );
+
+        file_put_contents($logFilePath, json_encode($logData, JSON_PRETTY_PRINT));
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $late = isLateMorning();
+
+    logStudent($name, $late);
+}
+
+function displayStudentData()
+{
+    global $logFilePath;
+    $studentData = file_exists($logFilePath) ? json_decode(file_get_contents($logFilePath), true) : [];
+    if (!empty($studentData)) {
+        foreach ($studentData as $student) {
+            echo "Name: " . $student['name'] . "\n";
+            echo "Time: " . $student['time'] . "\n";
+            echo "Late: " . ($student['late']) . "\n\n";
+        }
+    } else {
+        echo "No student data available";
+    }
+}
 
 ?>
 <!DOCTYPE html>
@@ -27,10 +68,21 @@ if (isset($_POST['name'])) {
 </head>
 <body>
 
-    <form method="post">
-        Name: <input type="text" name="name">
-        <input type="submit">
-    </form>
-
+<form method="post">
+    Name: <input type="text" name="name">
+    <input type="submit">
+</form>
+<h3>Student Data</h3>
+<form method="post">
+    <input type="hidden" name="action" value="display_data">
+    <input type="submit" value="Display Student Data">
+</form>
+<pre>
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['action']) && $_POST['action'] == 'display_data') {
+            displayStudentData();
+        }
+        ?>
+</pre>
 </body>
-
+</html>
